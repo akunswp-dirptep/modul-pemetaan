66f523e3eba9763a4deb061ee51384513ded5d52
akmal <akmalulimanliari10@gmail.com>
2026-02-24 11:15:19 +0700

    5.6.2d: Penyesuaian Fitur Persiapan Data Pembuatan ZNT

---
+++ test_folder/read_penilaiantanah.py
@@ -0,0 +1,73 @@
+
+import json
+import os
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import base64   
+
+# Cryptography Functions
+def generate_key():
+    password = 'bpnri-jakarta'
+    salt = b'Sisinga@2-Jakarta'  
+
+    # Derive proper key dari password
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(password.encode()))
+    return key
+
+def simpan_ke_bin(data_terenkripsi, nama_file):
+    """Menyimpan data bytes ke dalam file biner."""
+    try:
+        with open(nama_file, 'wb') as file:
+            file.write(data_terenkripsi)
+        print(f"Pesan berhasil disimpan ke {nama_file}")
+    except IOError as e:
+        print(f"Terjadi kesalahan saat menulis ke file: {e}")
+
+def baca_dari_bin(nama_file):
+    """Membaca data bytes dari file biner."""
+    data_terenkripsi = None
+    try:
+        with open(nama_file, 'rb') as file:
+            data_terenkripsi = file.read()
+        print(f"Pesan berhasil dibaca dari {nama_file}")
+        return data_terenkripsi
+    except IOError as e:
+        print(f"Terjadi kesalahan saat membaca file: {e}")
+        return None
+    
+def encrypt_message(message: str, key: bytes, path) -> bytes:
+    fernet = Fernet(key)
+    encrypted_message = fernet.encrypt(message.encode())
+    simpan_ke_bin(encrypted_message, path)
+    return encrypted_message
+
+def decrypt_message(key: bytes, path) -> str:
+    encrypted_message = baca_dari_bin(path)
+    fernet = Fernet(key)
+    decrypted_message = fernet.decrypt(encrypted_message).decode()
+    data = json.loads(decrypted_message)
+    return data
+
+def get_user_data():
+    config_path = r'C:\PenilaianTanah\config\penilaiantanah.bin'
+
+    try:
+        if os.path.exists(config_path):
+            data = decrypt_message(generate_key(), config_path) 
+            print(data)
+            return data
+        else:
+            return None
+        
+    except Exception as e:
+        print(f"Gagal membaca user config: {str(e)}")
+        return None
+
+get_user_data()

f8ced7c89dcf30a2084a236d3a8cff78017c78c4
akmal <akmalulimanliari10@gmail.com>
2026-04-07 09:29:43 +0700

    5.8.3d: Finalizing ReadMe and Version ID

--- README.txt
+++ README.txt
@@ -1,6 +1,6 @@
-Version: 5.8.2 - Jayawijaya
-Published: 02/04/2025
+Version: 5.8.3 - Jayawijaya
+Published: 07/04/2025
 Created by: Direktorat Penilaian Tanah dan Ekonomi Pertanahan Kementerian ATR/BPN
 
 Penyesuaian Fitur:
- - Penambahan ID perilisan sebagai data pembanding untuk mengecek kesesuai versi
+ - Penambahan Kab Takalar pada Kolom Kabupaten/Kota
--- scripts/Pengembangan Enkripsi/zntutils/constant.py
+++ scripts/Pengembangan Enkripsi/zntutils/constant.py
@@ -653,9 +653,9 @@ KAB_KOTA = {
     ],
 }
 
-VERSION_NUMBER = '5.8.2'
+VERSION_NUMBER = '5.8.3'
 VERSION_NAME = 'Jayawijaya'
-VERSION_ID = 'halus-cerdas-padang-69cdce82-i43'
+VERSION_ID = 'laptop-aktif-harddisk-69d46b4f-9bc'
 CURRENT_VERSION = f'{VERSION_NUMBER} - {VERSION_NAME}'
 SIPENTA_SERVER_INSTALLER_URL = 'https://belajar.atrbpn.go.id/sipenta/tatausaha/apis/installer'
 UPDATE_URL = "https://raw.githubusercontent.com/Akring-creator/update-version-repo/main/realease-notes.json"

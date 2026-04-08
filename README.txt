ac6a7a0bfc09ddaf58a2504531fb394cc4e2c794
akmal <akmalulimanliari10@gmail.com>
2026-04-08 11:15:10 +0700

    5.8.4: Finalizing README

--- README.txt
+++ README.txt
@@ -1,6 +1,6 @@
-Version: 5.8.3 - Jayawijaya
-Published: 07/04/2025
+Version: 5.8.4 - Jayawijaya
+Published: 08/04/2025
 Created by: Direktorat Penilaian Tanah dan Ekonomi Pertanahan Kementerian ATR/BPN
 
 Penyesuaian Fitur:
- - Penambahan Kab Takalar pada Kolom Kabupaten/Kota
+ - Penyesuaian Zero Titik Sampel Individual 
--- scripts/Pengembangan Enkripsi/zntutils/constant.py
+++ scripts/Pengembangan Enkripsi/zntutils/constant.py
@@ -653,9 +653,9 @@ KAB_KOTA = {
     ],
 }
 
-VERSION_NUMBER = '5.8.3'
+VERSION_NUMBER = '5.8.4'
 VERSION_NAME = 'Jayawijaya'
-VERSION_ID = 'laptop-aktif-harddisk-69d46b4f-9bc'
+VERSION_ID = 'ular-medan-aceh-69d5d5c2-jzc'
 CURRENT_VERSION = f'{VERSION_NUMBER} - {VERSION_NAME}'
 SIPENTA_SERVER_INSTALLER_URL = 'https://belajar.atrbpn.go.id/sipenta/tatausaha/apis/installer'
 UPDATE_URL = "https://raw.githubusercontent.com/Akring-creator/update-version-repo/main/realease-notes.json"
